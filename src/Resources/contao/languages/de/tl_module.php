6d7c7008b1a7e357c87106a5403336a133c35e94
Ben <83883581+vtxm-h@users.noreply.github.com>
2026-01-19 18:05:27 +0100

    Update tl_module.php

--- src/Resources/contao/languages/de/tl_module.php
+++ src/Resources/contao/languages/de/tl_module.php
@@ -1,11 +1,4 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_module']['page'] = [
-    'Seite',
-    'Bitte wähle die Seite aus.'
-];
-
-$GLOBALS['TL_LANG']['tl_module']['article'] = [
-    'Artikel',
-    'Bitte wähle den Artikel der gewählten Seite aus.'
-];
+$GLOBALS['TL_LANG']['tl_module']['page'] = ['Seite', 'Bitte wähle die Seite aus.'];
+$GLOBALS['TL_LANG']['tl_module']['article'] = ['Artikel', 'Bitte wähle den Artikel aus.'];
