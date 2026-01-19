49e25c1d5113dd19ca19d7019729c8bbdf136144
Ben <83883581+vtxm-h@users.noreply.github.com>
2026-01-19 17:49:33 +0100

    Update modules.php

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -1,10 +1,8 @@
 <?php
 
-// Modultitel/-beschreibung (array)
 $GLOBALS['TL_LANG']['FMD']['article_insert'] = [
   'Artikel einfügen',
   'Gibt die Inhaltselemente eines Artikels aus.',
 ];
 
-// Gruppentitel (string) – optional, nur wenn du eine eigene Gruppe verwendest
 $GLOBALS['TL_LANG']['FMD']['article_insert_group'] = 'Artikel-Insert';
