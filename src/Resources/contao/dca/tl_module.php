cc836e4e1e7be18a859479e9dc1974c5f6eed0db
Ben <83883581+vtxm-h@users.noreply.github.com>
2026-01-19 18:03:11 +0100

    Update tl_module.php

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -3,29 +3,21 @@
 use ArticleInsert\Dca\ArticleOptions;
 
 $GLOBALS['TL_DCA']['tl_module']['palettes']['article_insert']
-= '{title_legend},name,headline,type;{config_legend},page,article;{template_legend:hide},customTpl';
+    = '{title_legend},name,headline,type;{config_legend},page,article;{template_legend:hide},customTpl';
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['page'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_module']['page'],
+    'exclude'   => true,
     'inputType' => 'pageTree',
-    'eval'      => [
-        'mandatory' => true,
-        'fieldType' => 'radio',
-        'tl_class'  => 'w50',
-        'submitOnChange' => true, // wichtig
-    ],
+    'eval'      => ['mandatory' => true, 'fieldType' => 'radio', 'tl_class' => 'w50'],
     'sql'       => "int(10) unsigned NOT NULL default 0",
 ];
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['article'] = [
-    'label'     => &$GLOBALS['TL_LANG']['tl_module']['article'],
-    'inputType' => 'select',
+    'label'            => &$GLOBALS['TL_LANG']['tl_module']['article'],
+    'exclude'          => true,
+    'inputType'        => 'select',
     'options_callback' => [ArticleOptions::class, 'getArticlesByPage'],
-    'eval'      => [
-        'mandatory' => true,
-        'chosen'    => true,
-        'tl_class'  => 'w50',
-        'includeBlankOption' => true,
-    ],
-    'sql'       => "int(10) unsigned NOT NULL default 0",
+    'eval'             => ['mandatory' => true, 'chosen' => true, 'includeBlankOption' => true, 'tl_class' => 'w50'],
+    'sql'              => "int(10) unsigned NOT NULL default 0",
 ];
