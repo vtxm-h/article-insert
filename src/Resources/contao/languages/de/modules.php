ed064e32577a425ebb8846fde2390f1e6b534469
Ben <83883581+vtxm-h@users.noreply.github.com>
2026-01-19 14:47:12 +0100

    Update modules.php

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['TL_LANG']['FMD']['miscellaneous']['article_insert'] = [
+$GLOBALS['TL_LANG']['FMD']['application']['article_insert'] = [
     'Artikel einfügen',
     'Gibt die Inhaltselemente eines Artikels aus.'
 ];
