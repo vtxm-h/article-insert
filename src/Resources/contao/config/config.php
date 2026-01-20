26b25d62ed065ab792d320ce0dd307b1275fc182
Ben <83883581+vtxm-h@users.noreply.github.com>
2026-01-20 10:58:29 +0100

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,4 +2,3 @@
 
 $GLOBALS['FE_MOD']['article_insert_group']['article_insert']
     = \ArticleInsert\Module\ModuleArticleInsert::class;
-$GLOBALS['TL_JAVASCRIPT'][] = 'bundles/articleinsert/backend/article_insert.js|static';
