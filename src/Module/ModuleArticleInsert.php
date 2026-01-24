d130f3431ff5a7708447c52274f046e3a47c5eef
Ben <83883581+vtxm-h@users.noreply.github.com>
2026-01-24 22:45:47 +0100

    Update ModuleArticleInsert.php

--- src/Module/ModuleArticleInsert.php
+++ src/Module/ModuleArticleInsert.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ArticleInsert\Module;
+namespace Vendor\ArticleInsertBundle\Module;
 
 use Contao\ArticleModel;
 use Contao\ContentModel;
@@ -13,24 +13,25 @@ class ModuleArticleInsert extends Module
     protected function compile(): void
     {
         $articleId = (int) ($this->article ?? 0);
+
         if ($articleId <= 0) {
             $this->Template->content = '';
             return;
         }
 
         $article = ArticleModel::findByPk($articleId);
+
         if (null === $article) {
             $this->Template->content = '';
             return;
         }
 
-        // Inhaltelemente des Artikels holen und rendern
         $elements = ContentModel::findPublishedByPidAndTable($article->id, 'tl_article');
         $buffer = '';
 
         if (null !== $elements) {
-            foreach ($elements as $element) {
-                $buffer .= $this->getContentElement($element);
+            while ($elements->next()) {
+                $buffer .= $this->getContentElement($elements->id);
             }
         }
 
