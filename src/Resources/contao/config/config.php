61a93f16c2b0acee4ba90a7a3d98716e58c2242d
Ben <83883581+vtxm-h@users.noreply.github.com>
2026-01-24 22:37:08 +0100

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,4 +1,4 @@
 <?php
 
 $GLOBALS['FE_MOD']['article_insert_group']['article_insert']
-    = \ArticleInsert\Module\ModuleArticleInsert::class;
+    = \Vendor\ArticleInsertBundle\Module\ModuleArticleInsert::class;
